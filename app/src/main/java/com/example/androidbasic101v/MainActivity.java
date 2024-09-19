1ba38764146088ea1872e106c35b657ad86c6c12
jayed.nahian <jayed.nahian@trucklagbe>
2024-09-19 13:13:20 +0600

    45.- complete image tap and image change done

--- app/src/main/java/com/example/androidbasic101v/MainActivity.java
+++ app/src/main/java/com/example/androidbasic101v/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidbasic101v;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean imageOneShowing = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onPressImage(View view){
+        Log.i("info", "image tapped");
+        ImageView imageViewOne = (ImageView) findViewById(R.id.imageView2);
+        ImageView imageViewTwo = (ImageView) findViewById(R.id.imageView3);
+
+        if(imageOneShowing){
+            imageOneShowing =false;
+            Log.i("info", " 0 - 1 :"+ imageOneShowing );
+            imageViewOne.animate().alpha(0).setDuration(2000);
+            imageViewTwo.animate().alpha(1).setDuration(2000);
+        }
+        else {
+
+            imageOneShowing =true;
+            Log.i("info", " 1 - 0: "+imageOneShowing );
+            imageViewOne.animate().alpha(1).setDuration(2000);
+            imageViewTwo.animate().alpha(0).setDuration(2000);
+
+        }
+        ;
+//        imageViewTwo.animate().alpha(0).setDuration(2000);
+
+
+
+//        imageViewOne.animate().alpha(1).setDuration(2000);
+
+
     }
 }
